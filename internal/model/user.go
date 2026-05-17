4432f9fdca50d3f4faeeb02cf870a38c69ccc0a9
2296729555 <2296729555@qq.com>
2026-05-17 20:34:29 +0800

    Update User Model

--- internal/model/user.go
+++ internal/model/user.go
@@ -2,6 +2,6 @@ package model
 
 type User struct {
 	ID       uint   `gorm:"column:id;primarykey;autoIncrement"`
-	Username string `gorm:"column:username;unique" validate:"max=12"`
-	Password string `gorm:"column:password;NOT NULL" validate:"min=8,max=20"`
+	Username string `gorm:"column:username;unique"`
+	Password string `gorm:"column:password;NOT NULL"`
 }
