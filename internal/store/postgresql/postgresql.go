280df36c0abd6dad7623837456c75b1844af919f
2296729555 <2296729555@qq.com>
2026-05-17 21:56:56 +0800

    Add database source

---
+++ internal/store/postgresql/postgresql.go
@@ -0,0 +1,38 @@
+package postgresql
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func NewPostgres() *gorm.DB {
+
+	dsn := os.Getenv("POSTGRESQL_DSN")
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             time.Second,
+			LogLevel:                  logger.Error,
+			IgnoreRecordNotFoundError: true,
+			ParameterizedQueries:      true,
+			Colorful:                  true,
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+
+	if err != nil {
+		panic("连接postgresql失败")
+	}
+
+	log.Println("连接postgresql成功")
+	return db
+}
