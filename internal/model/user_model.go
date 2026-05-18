d948333ef423b33360ea0bc30187b09ed3773236
2296729555 <2296729555@qq.com>
2026-05-18 11:04:38 +0800

    Update user_model

--- internal/model/user_model.go
+++ internal/model/user_model.go
@@ -1,7 +1,11 @@
 package model
 
+import "time"
+
 type User struct {
-	ID       uint   `gorm:"column:id;primarykey;autoIncrement"`
-	Username string `gorm:"column:username;unique"`
-	Password string `gorm:"column:password;NOT NULL"`
+	ID        uint      `gorm:"column:id;primarykey;autoIncrement"`
+	Username  string    `gorm:"column:username;unique"`
+	Password  string    `gorm:"column:password;not null"`
+	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt time.Time `gorm:"column:updated_at;autoUpdateTime"`
 }
