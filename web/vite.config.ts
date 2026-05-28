faa12ae0c25668ecb071b8400d0a7505ef459d72
2296729555 <2296729555@qq.com>
2026-05-28 21:55:31 +0800

    Add a locally deployed Vite proxy

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -18,5 +18,13 @@ export default defineConfig({
   },
   server: {
     port: 5173,
+    proxy: {
+      // 当本地开发遇到 /v1 开头的请求时
+      '/v1': {
+        target: 'http://localhost:8080', // 转发给本地起起来的 Go 后端
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // 把 /api 去掉，变成 /v1/... 传给后端
+      }
+    }
   },
 })
